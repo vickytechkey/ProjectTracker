593054e8e45e2d6b56b95488078fbe723d890471
vickytechkey <vichunice@gmail.com>
2026-04-29 11:23:37 +0530

    Adding Database abstract

---
+++ db/DataBase.py
@@ -0,0 +1,23 @@
+from abc import ABC,abstractmethod
+
+class MasterDatabase(ABC):
+    
+    @abstractmethod
+    def CreateDB(self,dbname):
+        pass
+    
+    @abstractmethod
+    def Connection(self,query):
+        pass
+    
+    @abstractmethod
+    def CreateTable(self,query):
+        pass
+    
+    @abstractmethod
+    def QueryExecution(self,query):
+        pass
+    
+    @abstractmethod
+    def SelectQuery(self,query):
+        pass
