bb23d76ca4f7e9888d5c1f55935e26b2baa7bfa1
vickytechkey <vichunice@gmail.com>
2026-04-30 14:10:52 +0530

    test code of main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from db.sqlitedb import SqliteDB
 
 sql = SqliteDB("projecttracker.db")
 sql.CreateTable()
+insert_query = '''
+SELECT * FROM PROJECTS
+'''
+rows = sql.SelectQuery(insert_query)
+for row in rows:
+    print(row)
+
+
 
