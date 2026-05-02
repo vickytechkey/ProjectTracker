ee28233055f8f681e9ae8519a3242acbfef07aec
vickytechkey <vichunice@gmail.com>
2026-05-02 15:48:17 +0530

    adding test code

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from projectManagement.projects.resolve_project import ResolveProject
 from projectManagement.install import InstallationModule
+from projectManagement.projects.createproject import CreateProject
 
 i = InstallationModule()
+c = CreateProject("testproject2",{"start_date":"01-01-2025","end_date":"01-01-2026"},10)
+c.CreateProject()
+# r = ResolveProject(1)
+# r.CloseProject()
 
-r = ResolveProject(1)
-r.CloseProject()
 
 
 
