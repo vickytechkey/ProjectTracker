77304a772231fc28c57c00705189dad165b87b92
vickytechkey <vichunice@gmail.com>
2026-05-03 07:37:26 +0530

    Adding sub tasks

--- generic/generic_values.py
+++ generic/generic_values.py
@@ -1,3 +1,18 @@
+from DataFields.DataField import StringField
+import json
+
 class GenericValue:
     project_tracker_db = "projecttracker.db"
-    task_path = "./tasks/"
+    project_folder = "./projects/"
+
+
+class FileCreator:
+    Filename  = StringField()
+    def __init__(self,filename,content):
+        self.Filename = filename
+        self.content = content
+    
+    def CreateFile(self):
+        with open(self.Filename,"w+",encoding="utf-8") as f:
+            json.dump(self.content,f,indent=4)
+        print(self.Filename)
--- projectManagement/subtask/createSubtask.py
+++ projectManagement/subtask/createSubtask.py
@@ -1,7 +1,8 @@
 import json
 from datetime import date
 from DataFields.DataField import StringField,DateField,PositiveInteger,IntegerType
-from generic.generic_values import GenericValue
+from generic.generic_values import GenericValue,FileCreator
+from db.sqlitedb import SqliteDB
 
 
 class CreateSubTask:
@@ -10,7 +11,9 @@ class CreateSubTask:
     creationDate = date.today()
     planned_efforts = PositiveInteger()
     subtask_id = PositiveInteger()
-    def __init__(self,taskname,start_date,end_date,planned_efforts,subtask_id):
+    project_id = PositiveInteger()
+    db = SqliteDB(GenericValue.project_tracker_db)
+    def __init__(self,taskname,start_date,end_date,planned_efforts,subtask_id,project_id):
         self.subTaskname = taskname
         self.start_enddate = {
             "start_date":start_date,
@@ -18,6 +21,7 @@ class CreateSubTask:
         }
         self.planned_efforts = planned_efforts
         self.subtask_id = subtask_id
+        self.project_id = project_id
     
     def CreateSubTask(self):
         with open("./projectManagement/subtask/subtask_template.json","r+") as f:
@@ -30,7 +34,21 @@ class CreateSubTask:
         subtask_template["end_date"] = self.start_enddate["end_date"]
         subtask_template["planned_efforts"] = self.planned_efforts
         
-        task_path = GenericValue.task_path + self.subTaskname + ".json"
+        # getting project name using the project id
+        select_sql = f'''
+        SELECT PROJECT_NAME FROM PROJECTS WHERE PROJECT_ID={self.project_id}
+        '''
+        result = self.db.SelectQuery(select_sql)
+        project_name =  result[0][0]
+        # Reading test project file
+        project_file_name = f"./projects/{project_name}.json"
+        print(project_file_name)
+        with open(project_file_name,"r+") as f:
+            project_file = json.load(f)
+        # inserting the subtask in the project
+        project_file["sub_tasks"].append(subtask_template)
+        # editing the project file
+        with open(project_file_name,"w+") as f:
+            json.dump()
         
-        with open(task_path , "w+") as f :
-            json.dump(subtask_template, f, indent=4)
+        
