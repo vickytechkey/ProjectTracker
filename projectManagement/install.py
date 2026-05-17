285b68ad70994b0062ded43302da6a2cb4a565ea
vickytechkey <vichunice@gmail.com>
2026-05-17 21:59:38 +0530

    deleting installation as sqlite is removed

--- projectManagement/install.py
+++
@@ -1,8 +0,0 @@
-from DataFields.DataField import IntegerType,PositiveInteger,StringField,DateField,ProjectStatus
-from db.sqlitedb import SqliteDB
-from generic.generic_values import GenericValue
-
-class InstallationModule:
-    db = SqliteDB(GenericValue.project_tracker_db)
-    def __init__(self):
-        self.db.CreateTable()
