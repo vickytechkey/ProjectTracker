0c67b5656db14381ff842ba2cb395d0dc5e60e75
vickytechkey <vichunice@gmail.com>
2026-05-17 22:00:23 +0530

    testing new modules

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from db.mongo import MongoDB
-from projectManagement.projects.createproject import CreateProject
-from projectManagement.subtask.createSubtask import CreateSubTask
-from projectManagement.subtask.adding_correspondence import AddingMessage
-from projectManagement.projects.blocked_project import BlockProject
-from projectManagement.subtask.subtask_resolved import ResolvedSubtask
+from projectManagement.main import ResolvedSubtask,CreateProject,CreateSubTask,AddingMessage
 
 
-# c = CreateProject(projectName="Testproject",project_start_end_date={"start_date":"17-05-2026","end_date":"20-05-2026"},plannedEfforts=10)
+# c = CreateProject(projectName="Testproject1",project_start_end_date={"start_date":"17-05-2026","end_date":"20-05-2026"},plannedEfforts=10)
 # c.CreateProject()
 
-# s = CreateSubTask(taskname="planning",start_date="05-05-2026",end_date="05-06-2026",planned_efforts=10,project_name="Testproject")
-# s.CreateSubTask()
+s = CreateSubTask(taskname="planning",start_date="05-05-2026",end_date="05-06-2026",planned_efforts=10,project_name="Testproject")
+s.CreateSubTask()
 
-# c1 = AddingMessage(projectname="Testproject",subtaskname="planning",message="Test Message")
-# c1.createcorrespondence()
+c1 = AddingMessage(projectname="Testproject1",subtaskname="planning",message="Test Message")
+c1.createcorrespondence()
 
 # c2 = BlockProject(projectname="Testproject")
 # c2.blockproject()
 
-s = ResolvedSubtask(projectname="Testproject",subtaskname="planning")
-s.resolve_task()
+# s = ResolvedSubtask(projectname="Testproject",subtaskname="planning")
+# s.resolve_task()
 
 
 
