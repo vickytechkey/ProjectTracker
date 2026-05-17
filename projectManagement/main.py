418a2b7a196723e0aa8169cc0d43ddbaaeb6b73f
vickytechkey <vichunice@gmail.com>
2026-05-17 21:59:49 +0530

    adding common file

---
+++ projectManagement/main.py
@@ -0,0 +1,53 @@
+from projectManagement.projects.createproject import CreateProject
+from projectManagement.projects.blocked_project import BlockProject
+from projectManagement.projects.progress_project import ProgressProject
+from projectManagement.projects.resolve_project import ResolveProject
+from projectManagement.subtask.createSubtask import CreateSubTask
+from projectManagement.subtask.adding_correspondence import AddingMessage
+from projectManagement.subtask.subtask_blocked import BlockSubtask
+from projectManagement.subtask.subtask_resolved import ResolvedSubtask
+from projectManagement.subtask.subtask_progress import ProgressSubtask
+
+
+class CreateProject(CreateProject):
+    def __int__(self):
+        pass
+
+
+class BlockProject(BlockProject):
+    def __int__(self):
+        pass
+
+
+class ProgressProject(ProgressProject):
+    def __int__(self):
+        pass
+
+class ResolveProject(ResolveProject):
+    def __int__(self):
+        pass
+
+class CreateSubTask(CreateSubTask):
+    def __int__(self):
+        pass
+
+class AddingMessage(AddingMessage):
+    def __int__(self):
+        pass
+
+
+class BlockSubtask(BlockSubtask):
+    def __int__(self):
+        pass
+
+
+class ResolvedSubtask(ResolvedSubtask):
+    def __int__(self):
+        pass
+
+
+class ProgressSubtask(ProgressSubtask):
+    def __int__(self):
+        pass
+
+
