532c386410b8c1be24cc40ee4a7f1c7fcb38b68c
vickytechkey <vichunice@gmail.com>
2026-05-23 19:36:41 +0530

    deleting particles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,19 @@ import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div>
+   <>
+     
+
       <Navbar />
 
-      <h1>Welcome to Home Page</h1>
+      <div className="content">
+        <h1 style={{color:"white"}}>
+          Welcome
+        </h1>
+      </div>
+
       <Footer />
-    </div>
+    </>
   );
 }
 
