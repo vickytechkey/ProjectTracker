f84a9a4ac509c8999f957258e579a3d131cf994b
vickytechkey <vichunice@gmail.com>
2026-05-23 15:27:27 +0530

    adding footer in website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
 
       <h1>Welcome to Home Page</h1>
+      <Footer />
     </div>
   );
 }
